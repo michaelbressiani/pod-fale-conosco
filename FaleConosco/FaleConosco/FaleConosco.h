17cafaf49660c49cde8631a4a320d805b3ae389b
Michael Bressiani <>
2024-02-26 10:48:34 -0300

    Criando o arquivo framework do projeto

---
+++ FaleConosco/FaleConosco/FaleConosco.h
@@ -0,0 +1,18 @@
+//
+//  FaleConosco.h
+//  FaleConosco
+//
+//  Created by Michael Bressiani on 26/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FaleConosco.
+FOUNDATION_EXPORT double FaleConoscoVersionNumber;
+
+//! Project version string for FaleConosco.
+FOUNDATION_EXPORT const unsigned char FaleConoscoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FaleConosco/PublicHeader.h>
+
+
